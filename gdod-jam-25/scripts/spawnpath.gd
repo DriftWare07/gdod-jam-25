e0e4214fc93f86caf586c635fe8a73ad6dc4a180
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2025-03-07 08:49:05 -0600

    Difficulty tweaks

--- gdod-jam-25/scripts/spawnpath.gd
+++ gdod-jam-25/scripts/spawnpath.gd
@@ -9,10 +9,15 @@ extends PathFollow2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func spawn() -> void:
 	
-	var e = enemies.pick_random().instantiate()
-	progress_ratio = randf_range(0,1)
-	
-	$blood.restart()
-	$teleport.play()
-	get_parent().add_child(e)
-	e.global_position = global_position
+	var amount = randi_range(1,3)
+	for i in range(amount):
+		var e = enemies.pick_random().instantiate()
+		progress_ratio = randf_range(0,1)
+		
+		$blood.restart()
+		$teleport.play()
+		get_parent().add_child(e)
+		e.global_position = global_position
+		
+		$spawntimer.wait_time = 2.5 - (Global.TileMapParent.current_floor/5)
+		$spawntimer.wait_time = clamp($spawntimer.wait_time, 1.0, 5.0)
